f2d52f3b408eee4a97e1a6a4c7b5819b87966bd6
Lars Woxberg <lars.woxberg@gmail.com>
2015-04-27 17:48:36 +0200

    Initial commit.
    External libraries in place. Skeleton project in place.

---
+++ premake4.lua
@@ -0,0 +1,37 @@
+solution "car2d"
+    configurations { "Debug", "Release" }
+    platforms { "x32", "x64" }
+    location "build"
+    
+	configuration { "x32", "Debug" }
+		targetdir "bin/x86/debug/"
+        debugdir "bin/x86/debug/"
+        libdirs "external/lib/x86/"
+		flags { "Symbols" }
+	configuration { "x32", "Release" }
+		targetdir "bin/x86/release/"
+        debugdir "bin/x86/release/"
+        libdirs "external/lib/x86/"
+		flags { "Optimize" }
+	configuration { "x64", "Debug" }
+		targetdir "bin/x64/debug/"
+        debugdir "bin/x64/debug/"
+        libdirs "external/lib/x64/"
+		flags { "Symbols" }
+	configuration { "x64", "Release" }
+		targetdir "bin/x64/release/"
+        debugdir "bin/x64/release/"
+        libdirs "external/lib/x64/"
+		flags { "Optimize" }
+	configuration {}
+    
+    includedirs { "external/include/" }
+    buildoptions { "/W4" }
+        
+    project "car2d_main"
+        kind "ConsoleApp"
+        language "C++"
+        files { "code/car2d_main/**.hpp", "code/car2d_main/**.cpp" }
+        objdir "build/car2d_main/obj/"
+        links { "opengl32", "SDL2", "SDL2main", "glew32" }
+        
